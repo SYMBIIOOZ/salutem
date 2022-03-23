a9f2a3707f6413fd6bf28f0fd8520c1aed09447d
Dimitri LE GAL <legal.dimitri49@gmail.com>
2022-03-23 14:20:41 +0100

    install rector

---
+++ Entity/Doctor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Doctor
+ *
+ */
+#[ORM\Table(name: 'doctor', indexes: [new ORM\Index(name: 'fk_doctor_speciality_idx', columns: ['speciality_id'])])]
+#[ORM\Entity]
+class Doctor
+{
+    #[ORM\Column(name: 'id', type: 'bigint', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private int $id;
+
+    #[ORM\Column(name: 'first_name', type: 'string', length: 255, nullable: false)]
+    private string $firstName;
+
+    #[ORM\Column(name: 'last_name', type: 'string', length: 255, nullable: false)]
+    private string $lastName;
+
+    #[ORM\Column(name: 'photo', type: 'string', length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\Column(name: 'description', type: 'text', length: 65535, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(name: 'phone', type: 'string', length: 255, nullable: true)]
+    private ?string $phone = null;
+
+    #[ORM\Column(name: 'email', type: 'string', length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\ManyToOne(targetEntity: 'Entity\Speciality')]
+    #[ORM\JoinColumn(name: 'speciality_id', referencedColumnName: 'id')]
+    private \Entity\Speciality $speciality;
+
+
+}
---
+++ Entity/Speciality.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Speciality
+ *
+ */
+#[ORM\Table(name: 'speciality')]
+#[ORM\Entity]
+class Speciality
+{
+    #[ORM\Column(name: 'id', type: 'bigint', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private int $id;
+
+    #[ORM\Column(name: 'name', type: 'string', length: 255, nullable: false)]
+    private string $name;
+}
